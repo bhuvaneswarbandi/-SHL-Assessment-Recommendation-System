3a4b398f7daf475a4991bf7333ba40f604bda5ca
bhuvaneswarbandi <bhuvaneswarbandi@gmail.com>
2025-05-05 09:27:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,19 @@
 import os
-
-os.environ["STREAMLIT_SERVER_PORT"] = os.getenv("PORT", "8501")
-os.environ["STREAMLIT_SERVER_ENABLECORS"] = "false"
-os.environ["STREAMLIT_SERVER_HEADLESS"] = "true"
-
+import hashlib
 import streamlit as st
 from sentence_transformers import SentenceTransformer
 from qdrant_client import QdrantClient
-import hashlib
 from dotenv import load_dotenv
-import os
+
+# Set Streamlit environment configs
+os.environ["STREAMLIT_SERVER_PORT"] = os.getenv("PORT", "8501")
+os.environ["STREAMLIT_SERVER_ENABLECORS"] = "false"
+os.environ["STREAMLIT_SERVER_HEADLESS"] = "true"
+os.environ["STREAMLIT_SERVER_ADDRESS"] = "0.0.0.0"
 
 load_dotenv()
 
-
-#qdrant_api_url = os.getenv("QDRANT_API_URL")
-#qdrant_api_key = os.getenv("QDRANT_API_KEY")
-
-
 model = SentenceTransformer("all-MiniLM-L6-v2")
-
-
-qdrant_client = QdrantClient(
-    url="https://f9bb9b6d-494b-47fd-901a-521fe2270112.us-west-1-0.aws.cloud.qdrant.io:6333",
-    api_key="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3MiOiJtIn0.h2gYvCM_spmKzZqLB1pWMNgnFEsc8vMzzaoa3ZECV8I"
-)
-
 
 st.set_page_config(page_title="SHL", layout="centered")
 st.title(" SHL Assessment Recommendation System")
@@ -34,13 +22,19 @@ query = st.text_input("Enter a topic or keyword:", "")
 limit = st.slider("Select number of results", min_value=1, max_value=10, value=3)
 
 if st.button("Search") and query:
+    # Move Qdrant client here
+    qdrant_client = QdrantClient(
+        url="https://f9bb9b6d-494b-47fd-901a-521fe2270112.us-west-1-0.aws.cloud.qdrant.io:6333",
+        api_key="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3MiOiJtIn0.h2gYvCM_spmKzZqLB1pWMNgnFEsc8vMzzaoa3ZECV8I"
+    )
+
     query_vector = model.encode(query).tolist()
 
     with st.spinner("Searching Qdrant..."):
         raw_results = qdrant_client.search(
             collection_name="course_data",
-            query_vector=query_vector,  
-            limit=10,             
+            query_vector=query_vector,
+            limit=10,
             with_payload=True
         )
 
@@ -71,6 +65,5 @@ if st.button("Search") and query:
                     st.write(f"**Assessment Length**: {payload.get('Assessment length', 'N/A')}")
                     st.write(f"**remote_testing**: {payload.get('remote_testing','N/A')}")
                     st.write(f"**adaptive_irt**: {payload.get('adaptive_irt','N/A')}")
-
         else:
             st.warning("No unique results found.")
